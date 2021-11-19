19178263051239421227b20963207f289c713614
Rodrigo <rodrigopython16@gmail.com>
2021-11-19 16:45:36 -0300

    refactor: login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Login from './pages/Login'
-import Register from './pages/Register'
 
 import './css/App.css'
 
@@ -11,7 +10,6 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,53 +1,27 @@
 import React from 'react'
-import { Redirect, Link } from 'react-router-dom'
-
-import loginUser from '../services/loginUser'
 
 import HVEXLOGO from '../imgs/logo.svg'
 
 import '../css/Login.css'
 
 function Login () {
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [error, setError] = React.useState(null)
-  const [redirect, setRedirect] = React.useState(false)
-
-  const localUser = localStorage.getItem('user_logged')
-  if (redirect || localUser) return <Redirect to="/home" />
-
   return (
-    <div className="login">
-      <section className="login-container">
-        <img src={HVEXLOGO} />
+    <div className="login-container">
+      <main className="login-container__main">
+        <div>
+          <img src={HVEXLOGO} />
+        </div>
 
-        <input
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          placeholder="Email"
-          type="email"
-        />
+        <form className="login-container__form">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+          <button>Entrar</button>
+        </form>
 
-        <input
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          placeholder="Senha"
-          type="password"
-        />
-
-        {error && (
-          <p className="login-container__error">Email ou senha incorretos</p>
-        )}
-
-        <button
-          onClick={() => loginUser(email, password, setError, setRedirect)}
-          type="button"
-        >
-          LOGIN
-        </button>
-
-        <Link to="/register">REGISTRAR</Link>
-      </section>
+        <div className="login-container__register">
+          <button>Registrar</button>
+        </div>
+      </main>
     </div>
   )
 }
